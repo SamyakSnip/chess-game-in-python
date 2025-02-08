4e34838792b9fc8702a2526f81f45b1e46ae4e6a
SamyakSnip <i24ma027@amhd.svnit.ac.in>
2025-02-08 21:21:13 +0530

    assets downloades

--- src/piece.py
+++ src/piece.py
@@ -1,4 +1,4 @@
-
+import os
 
 class Piece:
     
@@ -11,8 +11,13 @@ class Piece:
         self.set_texture()
         self.texture_rect = texture_rect
     
-    def set_texture(self):
-        pass
+    def set_texture(self, size = 80):
+        self.texture = os.path.join(
+            f'assets/images/images-{size}px/{self.color}_{self.name}.png'
+        )
+        
+    def add_moves(self, move):
+        self.moves.append(move)
     
 class Pawn(Piece):
     
